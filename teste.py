df546b0680fbb22d0f962492ca717fb65a623155
Renan Moura Guimarães <44559854+RenanMGuimaraes@users.noreply.github.com>
2023-12-30 09:42:28 -0300

    eliminando CTK do front

--- teste.py
+++ teste.py
@@ -1,12 +1,23 @@
-from tkinter import *
-from tkinter import ttk
+import tkinter as tk
 
-root = Tk()
-root.title("Tk test")
-root.geometry("800x800")
+def toggle_button_state():
+    if check_var.get() == 1:  # If the checkbox is checked
+        button.config(state=tk.NORMAL)  # Enable the button
+    else:
+        button.config(state=tk.DISABLED)  # Disable the button
 
-frame_1 = ttk.Frame(root, relief="sunken", height="400", width="400").grid(row=0, column=0, rowspan=1, columnspan=1)
-frame_2 = ttk.Frame(frame_1, relief="sunken", height="200", width="200").grid(row=0, column=0, rowspan=1, columnspan=1)
-label_1 = ttk.Label(frame_2, text="Text").grid(row=0, column=0, sticky="N, E")
+root = tk.Tk()
+root.title("Checkbox and Button Example")
+
+# Variable to hold the state of the checkbox
+check_var = tk.IntVar()
+
+# Create a checkbox
+checkbox = tk.Checkbutton(root, text="Enable Button", variable=check_var, command=toggle_button_state)
+checkbox.pack(padx=10, pady=10)
+
+# Create a button initially disabled
+button = tk.Entry(root, state=tk.DISABLED)
+button.pack(padx=10, pady=10)
 
 root.mainloop()
